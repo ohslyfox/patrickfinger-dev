6eff73a0fcdd912551e2595dc5f3d96dcf790a25
ohslyfox <patricksfinger@gmail.com>
2023-04-01 14:24:35 -0700

    update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
-// @ts-check
-
-/**
- * @type {import('next').NextConfig}
- **/
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'out',
+  reactStrictMode: true,
+  output: "export",
 }
 
 module.exports = nextConfig
