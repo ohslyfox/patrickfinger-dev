e2c01f5a706bbfa049ebb58f1ca4db443d654bfd
ohslyfox <patricksfinger@gmail.com>
2023-04-16 14:18:37 -0700

    use cloudflare redirects

--- src/pages/lol.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-export default function LolRedirect() {
-    useEffect(() => {
-        window.location.assign("https://slyfox.lol");
-    }, []);
-
-    return <></>;
-}
--- src/pages/resume.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-export default function ResumeRedirect() {
-    useEffect(() => {
-        window.location.assign("./resume.pdf");
-    }, []);
-
-    return <></>;
-}
