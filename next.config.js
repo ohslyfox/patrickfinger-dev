fdf1a2e3f6c4b0d2a94f6a0e31c1edc14b0579a7
ohslyfox <patricksfinger@gmail.com>
2023-04-15 23:15:40 -0700

    update redirects

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    async redirects() {
-        return [
-            {
-                source: "/resume",
-                destination: "/resume.pdf",
-                permanent: true,
-            },
-            {
-                source: "/lol",
-                destination: "https://slyfox.lol",
-                permanent: true,
-            },
-        ];
-    },
 };
 
 module.exports = nextConfig;
