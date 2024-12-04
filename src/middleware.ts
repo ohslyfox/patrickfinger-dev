27bb192a155977aba4822ef2bdebfafef8c67cf6
ohslyfox <patricksfinger@gmail.com>
2024-12-03 16:23:11 -0800

    export config

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function onRequest(request: NextRequest) {
+export function middleware(request: NextRequest) {
   const userAgent = request.headers.get('User-Agent') || '';
   const isMobile = /Mobi/i.test(userAgent);
   if (isMobile) {
     return NextResponse.redirect(new URL("https://m.patrickfinger.dev"));
   }
+}
+
+
+export const config = {
+  matcher: '/',
 }
