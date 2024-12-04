2b25da35741de6425568d93ca396f7e3bd7de9c0
ohslyfox <patricksfinger@gmail.com>
2024-12-03 17:43:13 -0800

    use cf middleware

---
+++ functions/_middleware.js
@@ -0,0 +1,14 @@
+export async function onRequest(context) {
+    try {
+        const userAgent = context.request.headers.get("User-Agent") || "";
+        const isMobile = /Mobi/i.test(userAgent);
+        if (isMobile) {
+            return new Response(new URL("https://m.patrickfinger.dev"), {
+                status: 301,
+            });
+        }
+        return await context.next();
+    } catch (err) {
+        return new Response(`${err.message}`, { status: 500 });
+    }
+}
